0da5b375889d9db5d6235890a35c2f2b126021a8
HEXER22 <40181915+HEXER22@users.noreply.github.com>
2018-06-13 18:50:15 +0530

    Update hydra.sh

--- hydra.sh
+++ hydra.sh
@@ -3,10 +3,5 @@ apt-get update && apt-get upgrade -y
 
 pkg install hydra -y
 
-./configure
-make
-make install 
+echo "Successfully Installed HYDRA on Termux "
 
-apt-get install libssl-dev libssh-dev libidn11-dev libpcre3-dev \
-apt-get install libgit2.0-dev libmysqlclient-dev libeq-dev libsvn-dev \ 
-apt-get install firebird-dev libncp-dev
